d1d7d38df60b36ae23798c999124ff9ee9a10870
Siddharth Shukla <siddharthshukla@outlook.com>
2015-12-19 19:29:17 +0400

    modify user model add omniauth controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,23 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'hackkings-now.herokuapp.com' }
+
+
+
+  #General
+  config.app_domain = 'hacktbilisi-hext.herokuapp.com'
+  # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: config.app_domain }
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: '587',
+      enable_starttls_auto: true,
+      user_name: 'someuser',
+      password: 'somepass',
+      authentication: :plain,
+      domain: config.app_domain
+  }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   resources :questions, except: [:edit, :update] do
     post :send_sms, on: :collection
   end

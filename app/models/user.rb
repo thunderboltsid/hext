157a4105f9fd4679a246aad5e6507973d5c6140b
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2015-12-19 19:54:18 +0400

    Fixed alignment.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ActiveRecord::Base
   TEMP_EMAIL_PREFIX = 'change@me'
-  TEMP_EMAIL_REGEX = /\Achange@me/
+  TEMP_EMAIL_REGEX  = /\Achange@me/
 
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable
-  devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  devise  :database_authenticatable, :registerable, :confirmable,
+          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
 

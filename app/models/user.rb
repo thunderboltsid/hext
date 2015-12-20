205db40f628f344d80648c3b33cb26a17738e20a
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2015-12-20 10:20:38 +0400

    Added users controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
 
-  def find_for_oauth(auth, signed_in_resource = nil)
+  def self.find_for_oauth(auth, signed_in_resource = nil)
 
     # Get the identity and user if they exist
     identity = Identity.find_for_oauth(auth)
